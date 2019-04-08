ff7afb398c9cf6f82f1050cf9748a5e3780fe1d0
Infinidoge <sten.erickson04@gmail.com>
2019-04-08 13:54:24 -0400

    Moved War to a Function

--- War.py
+++ War.py
@@ -4,7 +4,9 @@ faces = list(range(2, 11))+["Jack", "Queen", "King", "Ace"]
 
 class Card:
     """
-    This happens to be a card
+    Card Object
+
+    Contains the Face, Suit, and Value of a Card
     """
   
     def __init__(self, face, suit):
@@ -32,6 +34,56 @@ for i in range(int(len(cards)/2)):
     player2.append(cards.pop(0))
 
 
+def war():
+    print("Both players declare war!\n")
+    down1 = []
+    down2 = []
+    for x in range(4):
+        if len(player1) < 1:
+            return True
+
+        if len(player2) < 1:
+            return True
+
+        down1.insert(0, player1.pop(0))
+        down2.insert(0, player2.pop(0))
+
+    print("Player1: " + str(down1[0]))
+    print("Player2: " + str(down2[0]))
+    while down1[0].value == down2[0].value:
+        for x in range(4):
+            if len(player1) < 1:
+                return True
+
+            if len(player2) < 1:
+                return True
+            down1.insert(0, player1.pop(0))
+            down2.insert(0, player2.pop(0))
+
+        print("Player1: " + str(down1[0]))
+        print("Player2: " + str(down2[0]))
+
+    if len(player1) < 1:
+        print("Player 2 has won via war")
+        return True
+
+    if len(player2) < 1:
+        print("Player 1 has won via war")
+        return True
+
+    if down1[0].value > down2[0].value:
+        player1.extend(down1)
+        player1.extend(down2)
+        print("Player 1 won the war\n")
+        return False
+
+    elif down1[0].value < down2[0].value:
+        player2.extend(down1)
+        player2.extend(down2)
+        print("Player 2 won the war\n")
+        return False
+
+
 input("\nPress enter to start war")
 while True:
   
@@ -45,63 +97,22 @@ while True:
   
     print("Player1: "+str(player1[0]))
     print("Player2: "+str(player2[0]))
+
     if player1[0].value > player2[0].value:
         print("Both players draw, but player one has taken player two's card\n")
         player1.append(player2.pop(0))
         player1.append(player1.pop(0))
+
     elif player1[0].value < player2[0].value:
         print("Both players draw, but player two has taken player one's card\n")
         player2.append(player1.pop(0))
         player2.append(player2.pop(0))
+
     elif player1[0].value == player2[0].value:
-        print("Both players declare war!\n")
-        down1 = []
-        down2 = []
-        for i in range(4):
-            if len(player1) < 1:
-                break
-          
-            if len(player2) < 1:
-                break
-            down1.insert(0, player1.pop(0))
-            down2.insert(0, player2.pop(0))
-
-        print("Player1: "+str(down1[0]))
-        print("Player2: "+str(down2[0]))
-        while down1[0].value == down2[0].value:
-            for i in range(4):
-                if len(player1) < 1:
-                    break
-              
-                if len(player2) < 1:
-                    break
-                down1.insert(0, player1.pop(0))
-                down2.insert(0, player2.pop(0))
-            
-            if len(player1) < 1:
-                break
-  
-            if len(player2) < 1:
-                break
-            print("Player1: "+str(down1[0]))
-            print("Player2: "+str(down2[0]))
-
-        if len(player1) < 1:
-            print("Player 2 has won via war")
-            break
-  
-        if len(player2) < 1:
-            print("Player 1 has won via war")
+        if war() is True:
             break
 
-        if down1[0].value > down2[0].value:
-            player1.extend(down1)
-            player1.extend(down2)
-            print("Player 1 won the war\n")
-        elif down1[0].value < down2[0].value:
-            player2.extend(down1)
-            player2.extend(down2)
-            print("Player 2 won the war\n")
     if testing is not True:
         input("Press enter to view the next duel")
+
 input("Press enter to finish")
