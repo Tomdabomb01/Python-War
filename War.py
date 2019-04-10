9283a11c9cc13ff448ed4c3dc5c78da0acbf4e13
Infinidoge <sten.erickson04@gmail.com>
2019-04-10 13:47:43 -0400

    Moved to importable function

--- War.py
+++ War.py
@@ -18,66 +18,65 @@ class Card:
         return str(self.face) + " of " + self.suit
 
 
-player1, player2 = (lambda deck:
-                    (deck[:len(deck)//2], deck[len(deck)//2:])
-                    )((lambda deck:
-                        random.sample(deck, len(deck)))(
-                        [Card(face, suit)
-                         for suit in ["Diamonds", "Spades", "Clubs", "Hearts"]
-                         for face in list(range(2, 11))+["Jack", "Queen", "King", "Ace"]]))
+def main():
+    player1, player2 = (lambda deck:
+                        (deck[:len(deck) // 2], deck[len(deck) // 2:])
+                        )((lambda deck:
+                           random.sample(deck, len(deck)))(
+        [Card(face, suit)
+         for suit in ["Diamonds", "Spades", "Clubs", "Hearts"]
+         for face in list(range(2, 11)) + ["Jack", "Queen", "King", "Ace"]]))
 
+    def battle():
+        print("Both players declare war!\n")
+        down1, down2 = [], []
 
-def battle():
-    print("Both players declare war!\n")
-    down1, down2 = [], []
+        while (len(down1) is 0 and len(down2) is 0) or down1[0].value == down2[0].value:
+            for x in range(4):
+                down1.insert(0, player1.pop(0))
+                down2.insert(0, player2.pop(0))
 
-    while (len(down1) is 0 and len(down2) is 0) or down1[0].value == down2[0].value:
-        for x in range(4):
-            down1.insert(0, player1.pop(0))
-            down2.insert(0, player2.pop(0))
+                if len(player1) < 1 or len(player2) < 1:
+                    return True
 
-            if len(player1) < 1 or len(player2) < 1:
-                return True
+            print("Player1: " + str(down1[0]))
+            print("Player2: " + str(down2[0]))
 
-        print("Player1: " + str(down1[0]))
-        print("Player2: " + str(down2[0]))
+        if down1[0].value > down2[0].value:
+            player1.extend(down1)
+            player1.extend(down2)
+            print("Player 1 won the war\n")
 
-    if down1[0].value > down2[0].value:
-        player1.extend(down1)
-        player1.extend(down2)
-        print("Player 1 won the war\n")
+        elif down1[0].value < down2[0].value:
+            player2.extend(down1)
+            player2.extend(down2)
+            print("Player 2 won the war\n")
 
-    elif down1[0].value < down2[0].value:
-        player2.extend(down1)
-        player2.extend(down2)
-        print("Player 2 won the war\n")
+        return False
 
-    return False
+    temp = input("\nPress enter to start war")
+    while len(player1) > 0 and len(player2) > 0:
 
+        print("Player 1: "+str(player1[0]))
+        print("Player 2: "+str(player2[0]))
 
-temp = input("\nPress enter to start war")
-while len(player1) > 0 and len(player2) > 0:
-  
-    print("Player 1: "+str(player1[0]))
-    print("Player 2: "+str(player2[0]))
+        if player1[0].value > player2[0].value:
+            print("Both players draw, but Player 1 has taken Player 2's card\n")
+            player1.append(player2.pop(0))
+            player1.append(player1.pop(0))
 
-    if player1[0].value > player2[0].value:
-        print("Both players draw, but Player 1 has taken Player 2's card\n")
-        player1.append(player2.pop(0))
-        player1.append(player1.pop(0))
+        elif player1[0].value < player2[0].value:
+            print("Both players draw, but Player 2 has taken Player 1's card\n")
+            player2.append(player1.pop(0))
+            player2.append(player2.pop(0))
 
-    elif player1[0].value < player2[0].value:
-        print("Both players draw, but Player 2 has taken Player 1's card\n")
-        player2.append(player1.pop(0))
-        player2.append(player2.pop(0))
+        elif player1[0].value == player2[0].value:
+            if battle():
+                break
 
-    elif player1[0].value == player2[0].value:
-        if battle():
-            break
+        if temp is not "fast":
+            input("Press enter to view the next duel")
 
-    if temp is not "fast":
-        input("Press enter to view the next duel")
+    print(f"Player {'1' if len(player2) < 1 else '2'} has won")
 
-print(f"Player {'1' if len(player2) < 1 else '2'} has won")
-
-input("Press enter to finish")
+    input("Press enter to finish")
